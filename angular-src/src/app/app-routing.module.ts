1c75c5de7bbca7a82047124c5e81d48ade51394b
geer1997 <germano.rojas@correo.unimet.edu.ve>
2018-02-14 12:59:54 -0400

    Ups

--- angular-src/src/app/app-routing.module.ts
+++ angular-src/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { ClientDashboardComponent } from './components/client-dashboard/client-dashboard.component';
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
-  { path: "login", component: LoginComponent }
+  { path: "login", component: LoginComponent },
+  { path: "client", component: ClientDashboardComponent}
 ];
 
 @NgModule({
