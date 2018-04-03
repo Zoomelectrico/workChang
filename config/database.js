d1ddf48b7084c112e2febff070e359e14b6d1ddc
Abraham Chang <abraham.chang@correo.unimet.edu.ve>
2018-04-03 19:17:40 -0400

    Manager Perfecto!

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 var Sequelize = require('sequelize');
 
-module.exports = new Sequelize('workchang', 'root', 'Paty1998', {
+module.exports = new Sequelize('workchang', 'root', '3364', {
   host: '127.0.0.1',
   dialect: 'mysql',
   port: 3306,
