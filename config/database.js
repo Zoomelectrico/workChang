afe8ce0caaaf8bb55cf7e2a617b8118a5cb02bcf
geer1997 <germano.rojas@correo.unimet.edu.ve>
2018-04-03 19:28:18 -0400

    Solucionar explosion

--- config/database.js
+++ config/database.js
@@ -1,10 +1,5 @@
 var Sequelize = require('sequelize');
-
-<<<<<<< HEAD
-module.exports = new Sequelize('workchang', 'root', '3364', {
-=======
 module.exports = new Sequelize('workchang', 'root', 'Germano1', {
->>>>>>> cefa9d75ed9495a9afef5d47792ceac814402c75
   host: '127.0.0.1',
   dialect: 'mysql',
   port: 3306,
